f40a8d35f187d7000f5865bc28ddca43d33c9f3b
Logarani Radhakrishnan <logarani.radhakrishnan@gmail.com>
2022-05-09 02:39:49 +0530

    commit

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const request = require("request")
+
+const dealer_url = "https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development/dealers"
+const vehicle_url = "https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development/vehicles/122348"
 
 const  credential = {
     username : "staff1",
@@ -18,8 +22,15 @@ router.post('/login', (req, res)=>{
 
 // route for dashboard
 router.get('/dashboard', (req, res) => {
-    if(req.session.user){
-        res.render('dashboard', {user : req.session.user})
+ if(req.session.user){
+
+        request.get(dealer_url, (error,response)=> {
+            const dealer_data = JSON.parse(response.body)
+            req.session.dealer_data = dealer_data[0].brand
+            console.log(req.session.dealer_data )
+        })
+
+        res.render('dashboard', {user : req.session.user},{dealer_data : req.session.dealer_data})
     }else{
         res.send("Unauthorized User")
     }
@@ -36,5 +47,29 @@ router.get('/logout', (req ,res)=>{
                  }
     })
 })
+
+
+
+/*
+
+request.get(dealer_url,function(req, res) {
+    const dealer_data = JSON.parse(res.body)
+    res.render('dashboard', { arr: dealer_data })
+    console.log(dealer_data)
+})
+        fetch(dealer_url)
+          .then(res => {
+            console.log(res.ok) // true
+            console.log(res.status) // 200
+            const dealer_data = JSON.parse(response.body)
+             console.log(dealer_data)
+             res.render('dashboard', {dealer_data : dealer_data})
+            return res.json()
+          })
+
+request({url : vehicle_url},(error,response) =>{
+    const data = JSON.parse(response.body)
+    console.log(data)
+})*/
 
 module.exports = router;
